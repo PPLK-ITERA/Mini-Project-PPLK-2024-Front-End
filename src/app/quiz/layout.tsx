d537265c08c322f735e37403bc9a8f0b44cc8f3f
jo0707 <josua123690707@gmail.com>
2024-06-27 23:21:16 +0700

    style: implementing design from vvd

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -5,7 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="h-full">
+      <Image src="/assets/background-quiz.png" className="fixed w-screen top-0 left-0 -z-50 -translate-y-1/2" quality={100} width={1000} height={500} alt="quiz-background" />
       {children}
     </div>
   );
