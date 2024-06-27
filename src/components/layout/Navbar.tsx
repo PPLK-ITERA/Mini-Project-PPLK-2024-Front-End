074a854e9d0019f8ddf7b4f721f76ee1e6fc914c
thebugitself <akhdan909@gmail.com>
2024-06-28 01:20:49 +0700

    style: fixed responsive nav

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -15,10 +15,10 @@ const Navbar = () => {
       <div>
         {isLoggedIn ? (
           <nav className="bg-gradient-to-r from-candlelight-800 to-candlelight-950 px-2 py-2 w-full">
-            <div className='flex flex-col md:flex-row justify-between items-center w-full'>
-              <h2 className={`${montserrat.className} text-white text-center md:text-left flex-grow mb-1 md:mb-0`}>Welcome, User</h2>
-              <h2 className={`${macondo.className} hidden md:block text-white text-center md:text-left flex-grow mb-1 md:mb-0`}>Pillar of Personality PPLK 2024</h2>
-              <Button className={`${montserrat.className} bg-transparent text-white flex items-center hover:bg-transparent mt-1 md:mt-0`} type="submit">
+            <div className='flex justify-between items-center w-full'>
+              <h2 className={`${montserrat.className} text-white text-center `}>Welcome, User</h2>
+              <h2 className={`${macondo.className} hidden md:block text-white text-center flex-grow mb-1 md:mb-0`}>Pillar of Personality PPLK 2024</h2>
+              <Button className={`${montserrat.className} bg-transparent text-white flex items-center hover:bg-transparent p-0`} type="submit">
                 <LogOut className="mr-2" />Keluar
               </Button>
             </div>
