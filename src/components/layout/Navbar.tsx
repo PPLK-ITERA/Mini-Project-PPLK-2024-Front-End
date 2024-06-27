bc4adf814a0ee94feba705f9e52ff2bd067075fe
thebugitself <akhdan909@gmail.com>
2024-06-28 01:00:24 +0700

    style: navbar responsive

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,19 +10,23 @@ const montserrat = Montserrat({subsets:["latin"]})
 const macondo = Macondo({weight:"400", subsets:["latin"]})
 
 const Navbar = () => {
-    const isLoggedIn = false;
+    const isLoggedIn = true;
     return (
-        <div>
-            {isLoggedIn ? (
-                <nav className="bg-gradient-to-r from-candlelight-800 to-candlelight-950 px-2 py-2 w-full">
-                  <div className='flex justify-between items-center w-full'>
-                    <h2 className={`${montserrat.className} text-white fg`}>Welcome, User</h2>
-                    <h2 className={`${macondo.className} hidden md:block text-white`}>Pillar of Personality PPLK 2024</h2>
-                    <Button className={`${montserrat.className} bg-transparent text-white flex items-center hover:bg-transparent `} type="submit"><LogOut className="mr-2" />Keluar</Button>
-                  </div>
-                </nav>
-            ) : null}
-        </div>
+      <div>
+        {isLoggedIn ? (
+          <nav className="bg-gradient-to-r from-candlelight-800 to-candlelight-950 px-2 py-2 w-full">
+            <div className='flex flex-col md:flex-row justify-between items-center w-full'>
+              <h2 className={`${montserrat.className} text-white text-center md:text-left mb-2 md:mb-0`}>Welcome, User</h2>
+              <h2 className={`${macondo.className} hidden md:block text-white text-center md:text-left mb-2 md:mb-0 md:ml-4`}>Pillar of Personality PPLK 2024</h2>
+              <Button className={`${montserrat.className} bg-transparent text-white flex items-center hover:bg-transparent mt-2 md:mt-0`} type="submit">
+                <LogOut className="mr-2" />Keluar
+              </Button>
+      </div>
+    </nav>
+  ) : null}
+</div>
+
+    
     );
   };
   
