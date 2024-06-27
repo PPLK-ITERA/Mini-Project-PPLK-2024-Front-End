6ee683b8738dc15c73db0b400f6f4b7ee249a8e5
thebugitself <akhdan909@gmail.com>
2024-06-27 21:45:48 +0700

    refactor: navigation bar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,22 +2,25 @@
 
 import React, { useState } from 'react';
 import { Button } from '../ui/button';
+import { Montserrat } from 'next/font/google';
+import { Macondo } from 'next/font/google';
+import { LogOut } from 'lucide-react';
+
+const montserrat = Montserrat({subsets:["latin"]})
+const macondo = Macondo({weight:"400", subsets:["latin"]})
 
 const Navbar = () => {
-    const isLoggedIn = false;
+    const isLoggedIn = true;
     return (
-      <nav className="bg-blue-400 px-2 py-2 w-full">
+      <nav className="bg-gradient-to-r from-candlelight-800 to-candlelight-950 px-2 py-2 w-full">
         <div>
             {isLoggedIn ? (
                 <div className='flex justify-between items-center w-full'>
-                  <h2 className='font-bold'>Website Kuis PPLK</h2>
-                  <Button className="px-4 rounded-md" type="submit">Logout</Button>
+                  <h2 className={`${montserrat.className} text-white fg`}>Welcome, User</h2>
+                  <h2 className={`${macondo.className} hidden md:block text-white`}>Pillar of Personality PPLK 2024</h2>
+                  <Button className={`${montserrat.className} bg-transparent text-white flex items-center hover:bg-transparent `} type="submit"><LogOut className="mr-2" />Keluar</Button>
                 </div>
-            ) : (
-                <div className='flex justify-center items-center w-full'>
-                    <h2 className='font-bold'>Website Kuis PPLK</h2>
-                </div>
-            )}
+            ) : null}
         </div>
       </nav>
     );
