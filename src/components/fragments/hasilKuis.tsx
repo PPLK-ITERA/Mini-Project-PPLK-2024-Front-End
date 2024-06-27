811355a37d8235fcdbff96aa471f14501b87ef59
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-06-28 02:04:48 +0700

    feat:slicing login ui

--- src/components/fragments/hasilKuis.tsx
+++ src/components/fragments/hasilKuis.tsx
@@ -6,34 +6,35 @@ const HasilKuis = () => {
   return (
     <div className="flex justify-center items-center align-center min-h-screen ">
       <div className="max-w-xl border border-black  rounded-lg px-12 py-8">
-        <PilarKeBerapa angka={1} className="flex justify-center items-center text-center pb-8" />
+        <PilarKeBerapa
+          angka={1}
+          className="flex justify-center items-center text-center pb-8"
+        />
 
         <div>
           <div className="flex gap-2 justify-center items-center">
-            <Pilar angka={1}  />
-            <DiagramBatang  />
+            <Pilar angka={1} />
+            <DiagramBatang />
           </div>
 
           <div className="flex gap-2 justify-center items-center">
-            <Pilar angka={2}  />
-            <DiagramBatang  />
+            <Pilar angka={2} />
+            <DiagramBatang />
           </div>
 
           <div className="flex gap-2 justify-center items-center">
-            <Pilar angka={3}  />
-            <DiagramBatang  />
+            <Pilar angka={3} />
+            <DiagramBatang />
           </div>
 
           <div className="flex gap-2 justify-center items-center">
-            <Pilar angka={4}  />
-            <DiagramBatang  />
+            <Pilar angka={4} />
+            <DiagramBatang />
           </div>
         </div>
       </div>
     </div>
   );
 };
-
-
 
 export default HasilKuis;
