393bfa5508ad9d60f36833122d148fd802506571
jo0707 <josua123690707@gmail.com>
2024-06-28 02:53:00 +0700

    fix: submit button icon

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight, Flag } from 'lucide-react';
 import clsx from 'clsx';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
@@ -106,7 +106,9 @@ export default function Page() {
           className="w-full bg-jaffa-600 my-8"
         >
           {currentQuestion < quizData.length - 1 ? 'Next' : 'Submit'}
-          <ArrowRight />
+          {currentQuestion < quizData.length - 1 ? <ArrowRight className="ml-2" /> : <svg className="ml-2" width="11" height="13" viewBox="0 0 11 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.5 1.62503C0.500012 1.54192 0.516601 1.45964 0.548796 1.38302C0.580991 1.30639 0.628146 1.23696 0.6875 1.17878C1.36865 0.511053 2.27866 0.127987 3.23229 0.107551C4.18592 0.0871153 5.11149 0.430846 5.82063 1.06878L6.04 1.27378C6.49589 1.67322 7.08138 1.89342 7.6875 1.89342C8.29363 1.89342 8.87911 1.67322 9.335 1.27378L9.49063 1.1319C9.87188 0.83003 10.445 1.07128 10.4969 1.5544L10.5 1.62503V7.25003C10.5 7.33314 10.4834 7.41542 10.4512 7.49204C10.419 7.56867 10.3719 7.6381 10.3125 7.69628C9.63135 8.36401 8.72134 8.74707 7.76772 8.76751C6.81409 8.78794 5.88851 8.44421 5.17938 7.80628L4.96 7.60128C4.51846 7.21441 3.95486 6.99526 3.36795 6.98223C2.78104 6.9692 2.20827 7.16313 1.75 7.53003V11.625C1.74982 11.7843 1.68883 11.9376 1.57947 12.0534C1.47011 12.1692 1.32065 12.2389 1.16163 12.2483C1.0026 12.2576 0.846013 12.2059 0.723855 12.1036C0.601697 12.0014 0.52319 11.8563 0.504375 11.6982L0.5 11.625V1.62503Z" fill="white"/>
+</svg>}
         </Button>
     </div>
   );
