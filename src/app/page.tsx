e60b5516480a5ad04ac2e33beb143a7b9735f607
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-06-27 14:51:54 +0700

    feat:add login feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import LoginForm from "@/components/layout/LoginForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Tes bang</h1>
-    </main>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <LoginForm />
+    </div>
   );
 }
