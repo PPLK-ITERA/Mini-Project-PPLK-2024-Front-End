193052d4bc02292a5ec7330403cfc93e62d929ec
Wupxy <muhammad.122140193@student.itera.ac.id>
2024-06-28 19:36:00 +0700

    feat:funfact

--- src/lib/data/funfact.ts
+++ src/lib/data/funfact.ts
@@ -20,3 +20,226 @@ export const FunFact = [
     desc: "Kata ini sering disisipkan di awal dan akhir kalimat dan berfungsi sama dengan kata 'dong' di dalam bahasa Indonesia.",
   },
 ];
+
+export const DATA_FUNFACT = [
+  {
+    nama: "Nayla",
+    funfact: [
+      {
+        title: "UKM Perwakilan Daerah",
+        subtitle: "Keragaman Budaya di ITERA",
+        desc: "ITERA memiliki Unit Kegiatan Mahasiswa yang masing-masing mewakili provinsi di Sumatera dan sekitarnya untuk mempererat kekeluargaan mahasiswa asal daerah.",
+      },
+      {
+        title: "Kolaborasi Kuliner",
+        subtitle: "Warung Makan Unik di BELWIS",
+        desc: "Beberapa program studi di ITERA bekerja sama dengan rumah makan kecil di daerah BELWIS (Belakang Wisma). Misalnnya, Warsin (Warung Mesin), Wardat (Warung Data)",
+      },
+      {
+        title: "Akun Menfess ITERA",
+        subtitle: "Wadah Aspirasi dan Informasi",
+        desc: "ITERA memiliki akun menfess di X yang berfungsi sebagai wadah bertanya, menyampaikan informasi, promosi jualan, sampai ajang untuk confess loh! Akun ini juga sudah pernah di suspend 2x.",
+      },
+    ],
+  },
+  {
+    nama: "Yesa",
+    funfact: [
+      {
+        title: "Prodi Unik di Indonesia",
+        subtitle: "Keunggulan ITERA",
+        desc: "ITERA memiliki beberapa program study yang cukup unik yang ternyata hanya ada satu loh di Indonesia beberapa prodi tersebut yaitu Prodi Rekayasa Kosmetik, Rekayasa Minyak dan Gas, dan Rekayasa Keolahragaan.",
+      },
+      {
+        title: "Observatorium Astronomi Lampung (OAIL)",
+        subtitle: "Teleskop Robotik Pertama di Asia Tenggara",
+        desc: "ITERA memiliki Observatorium Astronomi Lampung (OAIL) dengan teleskop robotik pertama yang ada di Asia Tenggara.",
+      },
+      {
+        title: "Lahan Kampus yang Luas",
+        subtitle: "Potensi Pengembangan ITERA",
+        desc: "ITERA memiliki lahan yang sanagat luas loh, yaitu sebesar 275 ha.",
+      },
+    ],
+  },
+  {
+    nama: "Cici",
+    funfact: [
+      {
+        title: "Prodi Sains Atmosfer dan Keplanetan (SAP)",
+        subtitle: "Satu-satunya di Indonesia",
+        desc: "Prodi Sains Atmosfer dan Keplanetan(SAP) hanya ada di ITERA loh!",
+      },
+      {
+        title: "Lokasi Awal ITERA",
+        subtitle: "Rencana Pembangunan di Sumatera Selatan",
+        desc: "Sebelum di provinsi Lampung, ITERA rencana dibangun nya di Sumatera Selatan tepatnya di daerah Talang Betutu.",
+      },
+      {
+        title: "Layanan Konseling Gratis",
+        subtitle: "Kesehatan Mental Civitas Akademika",
+        desc: "Tau ga sih? Kalau di ITERA punya layanan konseling bersama psikolog langsung loh dan gratis untuk semua civitas kampus.",
+      },
+    ],
+  },
+  {
+    nama: "Sigit",
+    funfact: [
+      {
+        title: "Bus Tayo ITERA",
+        subtitle: "Transportasi Mahasiswa Asrama",
+        desc: "ITERA memiliki bus tayo yang memiliki jadwal mengantar jemput Mahasiswa, Khususnya anak anak asrama.",
+      },
+      {
+        title: "Labirin Kebun Raya ITERA",
+        subtitle: "Petualangan Seru di Kampus",
+        desc: "ITERA merupakan kampus pertama di lampung yang memiliki labirin, labirin itu terletak di Kebun Raya ITERA.",
+      },
+      {
+        title: "OZT Award",
+        subtitle: "Penghargaan Mahasiswa TPB",
+        desc: "ITERA pada setiap akhir masa TPB memiliki sebuah penghargaan kepada Mahasiswa TPB. Penghargaan itu bernama OZT AWARD yang diambil dari nama alm. Rektor pertama ITERA.",
+      },
+    ],
+  },
+  {
+    nama: "Helen",
+    funfact: [
+      {
+        title: "Potensi Energi Sumatera",
+        subtitle: "Mengembangkan Sumber Daya Alam untuk Masa Depan Energi",
+        desc: "ITERA didirikan karena potensi energi besar di Sumatera seperti minyak, gas, batubara, dan panas bumi.",
+      },
+      {
+        title: "ITERA, Anak ITB",
+        subtitle: "Kontribusi dalam Pendidikan Bermutu di Indonesia",
+        desc: "ITERA merupakan anak dari ITB, berkontribusi dalam pendidikan bermutu di Indonesia.",
+      },
+      {
+        title: "Embung-Embung di ITERA",
+        subtitle: "Tempat Rekreasi dan Relaksasi di Kampus",
+        desc: "ITERA memiliki 7 embung untuk bersantai dan jogging, termasuk embung Kebun Raya dengan monumen berbentuk Sumatera.",
+      },
+    ],
+  },
+  {
+    nama: "Aulia",
+    funfact: [
+      {
+        title: "Fakultas Sains di ITERA",
+        subtitle: "Lebih dari Sekadar Teknik",
+        desc: "Walaupun Kampus Teknik of course kita juga ada Fakultas Sains loh!",
+      },
+      {
+        title: "Lahan Luas ITERA",
+        subtitle: "Kampus dengan Area yang Luas dan Hijau",
+        desc: "Itera punya lahan yang kira kira luasnya bisa sampe 275 hektar (bayangin deh seluas apa?)",
+      },
+      {
+        title: "Spot Studi dan Wisata",
+        subtitle: "Objek Studi Menarik di ITERA",
+        desc: "Tau ga si? Kalau itera itu punya spot titik yang bisa dijadiin objek studi ataupun wisata?",
+      },
+    ],
+  },
+  {
+    nama: "Silvia",
+    funfact: [
+      {
+        title: "Teleskop Canggih ITERA",
+        subtitle: "Menjelajah Luar Angkasa dari Kampus",
+        desc: "ITERA memiliki teleskop yang canggih untuk memantau luar angkasa",
+      },
+      {
+        title: "Ospek dan Konser",
+        subtitle: "Pengalaman Unik di ITERA",
+        desc: "Salah satu kampus yang ngadain ospek tapi bonus nya bisa ngonser juga",
+      },
+      {
+        title: "Laboratorium Lengkap",
+        subtitle: "Fasilitas Penunjang Pendidikan di ITERA",
+        desc: "Memiliki laboraturium prodi lengkap, seperti IOT, Laboratium Sipil dll",
+      },
+    ],
+  },
+  {
+    nama: "Ayu",
+    funfact: [
+      {
+        title: "Tahap Persiapan Bersama (TPB)",
+        subtitle: "Fondasi Awal Mahasiswa Baru ITERA",
+        desc: "Tahun pertama di ITERA melalui TPB untuk menyamaratakan pengetahuan mahasiswa sebelum memasuki penjurusan.",
+      },
+      {
+        title: "Kampus Hijau ITERA",
+        subtitle: "Program Penanaman Pohon dan Area Terbuka Hijau",
+        desc: "ITERA menjalankan program penanaman pohon untuk menciptakan lingkungan kampus yang hijau dan nyaman.",
+      },
+      {
+        title: "Dosen-Dosen Muda",
+        subtitle: "Lingkungan Belajar yang Aktif dan Kreatif",
+        desc: "ITERA memiliki banyak dosen muda yang membuat lingkungan belajar lebih aktif dan kreatif.",
+      },
+    ],
+  },
+  {
+    nama: "Cindy",
+    funfact: [
+      {
+        title: "Satu-satunya Institut Teknik di Sumatera",
+        subtitle: "ITERA, Kebanggaan Sumatera",
+        desc: "ITERA adalah satu-satunya perguruan tinggi institut teknik di Sumatera.",
+      },
+      {
+        title: "Galeri ITERA",
+        subtitle: "Minimarket Kampus dengan Segala Kebutuhan",
+        desc: "Galeri ITERA adalah minimarket di kampus yang menjual makanan, minuman, dan alat tulis.",
+      },
+      {
+        title: "Pembangkit Listrik Tenaga Surya",
+        subtitle: "Laboratorium PLTS Terbesar di Indonesia",
+        desc: "ITERA memiliki PLTS terbesar di Indonesia, seluas 1 hektare dan berkapasitas 1MWP.",
+      },
+    ],
+  },
+  {
+    nama: "Rachel",
+    funfact: [
+      {
+        title: "Program Studi Aktuaria",
+        subtitle: "Satu-Satunya di Sumatera",
+        desc: "ITERA adalah satu satunya kampus yang memiliki program studi Aktuaria di Sumatera.",
+      },
+      {
+        title: "Kebun Raya ITERA",
+        subtitle: "Konservasi Tumbuhan dan Wisata Labirin",
+        desc: "ITERA juga memiliki Kebun Raya sebagai fasilitas pendukung konservasi tumbuhan yang dapat dikunjungi masyarakat umum. Kebun Raya ITERA punya labirin juga loh!",
+      },
+      {
+        title: "Kontes Robot Terbang",
+        subtitle: "ITERA Tuan Rumah 2023",
+        desc: "Pada 2023 ITERA sukses jadi tuan rumah untuk Kontes Robot Terbang Indonesia",
+      },
+    ],
+  },
+  {
+    nama: "Doni",
+    funfact: [
+      {
+        title: "Gedung Serbaguna (GSG)",
+        subtitle: "Fasilitas Tersembunyi di ITERA",
+        desc: "ITERA memiliki Gedung Serbaguna (GSG) yang tersembunyi.",
+      },
+      {
+        title: "Masjid Raya At-Tanwir",
+        subtitle: "Masjid Unik dengan Batu Sungai",
+        desc: "ITERA memiliki fasilitas Masjid yang unik karena dindingnya tersusun atas batu-batu sungai yang dinamakan Masjid Raya At-Tanwir",
+      },
+      {
+        title: "Rumah Ibadah Multi Agama (RIMA)",
+        subtitle: "Fasilitas untuk Berbagai Agama di ITERA",
+        desc: "ITERA memiliki fasilitas RIMA (Rumah Ibadah Multi Agama) Fasilitas ini terdiri dari 2 area utama, yaitu area aula dan area agama yang terdiri dari 6 ruangan agama yaitu Islam, Protestan, Katolik, Hindu, Buddha, dan Konghucu.",
+      },
+    ],
+  },
+];
